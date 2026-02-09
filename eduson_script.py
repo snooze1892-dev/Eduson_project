7b91a2a230f27ebcbeae5217e8ce98703bedbfec
Дмитрий Барабанов <dmitrijbarabanov@MacBook-Air-Dmitrij.local>
2026-02-09 11:51:28 +0300

    Initial commit by eduson

---
+++ eduson_script.py
@@ -0,0 +1,13 @@
+MY_CONSTANT = 'Это результат'
+
+
+def this_is_function():
+    first_variable = 2
+    second_variable = 4
+    third_variable = first_variable + second_variable
+    return MY_CONSTANT, third_variable
+
+print(this_is_function())
+
+
+
